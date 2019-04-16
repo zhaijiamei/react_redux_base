a1e5ad60e43e86d583a123f16d4cfa2bd5808b86
zhaijiamei <269295616@qq.com>
2019-04-16 14:18:48 +0800

    hehehe0.0

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,85 +1,3 @@
-// var webpack = require('webpack')
-// const path = require('path');
-// var HtmlWebpackPlugin = require('html-webpack-plugin');
-
-// module.exports = {
-//     /*入口*/
-//     entry:{
-//         app:[
-//         'react-hot-loader/patch',
-//         path.join(__dirname, 'src/index.js')],
-
-//         vendor: ['react', 'react-router-dom', 'redux', 'react-dom']
-//         //app:path.join(__dirname, 'src/index.js'),
-//         //app:'src/index.js'
-       
-//     }, 
-    
-//     /*输出到dist文件夹，输出文件名字后面加哈希串.js*/
-//     output: {
-//         path: path.join(__dirname, './dist'),
-//         filename: '[name].[hash].js',
-//     },
-//     module: {
-//         /* rules各种加载器 */
-//         rules: [{
-//             test: /\.js$/,
-//             use: ['babel-loader?cacheDirectory=true'],
-//             include: path.join(__dirname, 'src')
-//         },{
-//             test: /\.css$/,
-//             use: ['style-loader', 'css-loader']
-//         },{
-//             test: /\.(png|jpg|gif)$/,
-//             use: [{
-//                 loader: 'url-loader',
-//                 options: {
-//                     limit: 8192
-//                 }
-//             }]
-//         }]
-//     },
-//     /*本地服务*/
-//     devServer:{
-//         contentBase:path.join(__dirname,'./dist'),
-//         hot: false,
-//         port:8089,
-//         historyApiFallback:true
-//     },
-//     //配置别名，从根目录开始
-//     resolve:{
-//         alias:{
-//             Pages:path.join(__dirname,'src/Pages'),
-//             Component:path.join(__dirname,'src/Component'),
-//             Router:path.join(__dirname,'src/router')
-//         }
-//     },
-//     /*生成source-map 用于查看源码 */
-//     devtool: 'inline-source-map',
-
-  
-//     plugins: [
-//     /*将编译后的js插入模板index.html中 */
-//         new HtmlWebpackPlugin({
-//         filename: 'index.html',
-//         template: path.join(__dirname, 'src/index.html')
-//      }),
-//    ],
-//    optimization: {
-//     //抽取公共的dm
-//         splitChunks: {
-//             cacheGroups: {
-//                 vendor: {
-//                    name: "vendor",
-//                    chunks: "initial",
-//                     minChunks: 2
-//                 }
-//             }
-//         }
-//     },
-
-// }
-
 const merge = require('webpack-merge');
 const path = require('path');
 const commonConfig = require('./webpack.base.config.js');
@@ -108,7 +26,7 @@ const devConfig = {
         open: true,
         // proxy: [{
         //   context: ['/bankPoint'],
-        //   target: 'http://xcodes.offline.site.fws.qa.nt.ctripcorp.com',
+        //   target: '',
         //   changeOrigin: true
         // }]
     },
